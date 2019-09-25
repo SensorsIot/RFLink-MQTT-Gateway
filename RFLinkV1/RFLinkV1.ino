8c3fa0ed4abddd2a810f36d24bff2ea37be8171a
couin3 <35873376+couin3@users.noreply.github.com>
2019-09-25 21:13:38 +0200

    Removed hi String
    Removed hi String, making direct use of msg[].
    Still playing with delay to maximize power efficiency

--- RFLinkV1/RFLinkV1.ino
+++ RFLinkV1/RFLinkV1.ino
@@ -5,11 +5,6 @@
   with the ESP8266 board/library.
 
   It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
 
   It will reconnect to the server if the connection is lost using a blocking
   reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
@@ -25,128 +20,108 @@
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#include <credentials.h>
-
-// Update these with values suitable for your network.
-
-const char* ssid = mySSID;
-const char* password = myPASSWORD;
-const char* mqtt_server = "192.168.0.203";
-const char* MQTT_USER = "admin";
-const char* MQTT_PASSWORD = "admin";
-const char* MQTT_TOPIC_REC = "RFLINK_REC";
-const char* MQTT_TOPIC_TRANS = "RFLINK_TRANS";
-
-String hi = "";
-bool dataAvailable = false;
-
+#include "credentials.h"
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-long lastMsg = 0;
-char msg[50];
-int value = 0;
+
+#define BAUD 57600
+#define MAX_MSG_SIZE 60
+
+char msg[MAX_MSG_SIZE + 1];
+byte msg_pos;
 
 void setup_wifi() {
 
   delay(10);
   // We start by connecting to a WiFi network
-  //Serial.println();
-  //Serial.print("Connecting to ");
-  //Serial.println(ssid);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
   WiFi.persistent(false);
   WiFi.setAutoReconnect(true);
   WiFi.setSleepMode(WIFI_MODEM_SLEEP); // Default is WIFI_NONE_SLEEP
-  // WiFi.setOutputPower(10); // 0~20.5dBm, Default max 
+  WiFi.setOutputPower(10); // 0~20.5dBm, Default max
 
   WiFi.mode(WIFI_STA);
-  // WiFi.config(ip, gateway, subnet); // For Static IP
+  WiFi.config(ip, gateway, subnet); // For Static IP
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    //Serial.print(".");
+    Serial.print(".");
   }
 
-  randomSeed(micros());
-
-  //Serial.println("");
-  //Serial.println("WiFi connected");
-  //Serial.println("IP address: ");
-  //Serial.println(WiFi.localIP());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
+/*
+  void callback(char* topic, byte* payload, unsigned int length) {
   //Serial.print("Message arrived [");
   //Serial.print(topic);
   //Serial.print("] ");
-  for (unsigned int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++) {
     Serial.write(payload[i]);
   }
   Serial.write('\n');
   //Serial.println();
-
-}
+  }
+*/
 
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
-    //Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (client.connect(clientId.c_str(), MQTT_USER, MQTT_PASSWORD)) {
-      //Serial.println("connected");
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect(MQTT_ID, MQTT_USER, MQTT_PSWD)) {
+      Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish(MQTT_TOPIC_REC, "hello world");
+      // client.publish(MQTT_TOPIC_OUT, "hello world");
       // ... and resubscribe
-      client.subscribe(MQTT_TOPIC_TRANS);
+      // client.subscribe(MQTT_TOPIC_IN);
     } else {
-      //Serial.print("failed, rc=");
-      //Serial.print(client.state());
-      //Serial.println(" try again in 5 seconds");
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
-      for (byte i=0; i<10; i++) delay(500); // delay(5000) may cause hang
+      for (byte i = 0; i < 10; i++) delay(500); // delay(5000) may cause hang
     }
   }
 }
 
 void setup() {
-  Serial.begin(57600);
+  Serial.begin(BAUD, SERIAL_8N1, SERIAL_FULL);
   setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  client.setServer(MQTT_SERVER, 1883);
+  // client.setCallback(callback);
 }
 
 void loop() {
-  char serialChar;
 
-  while (Serial.available() > 0) // Chek for availablity of data at Serial Port
+  msg_pos = 0;
+
+  while ((Serial.available() > 0) && (msg_pos < MAX_MSG_SIZE)) // Chek for availablity of data at Serial Port
   {
-    serialChar = Serial.read(); // Reading Serial Data and saving in data variable
-    String data2(serialChar);
-    hi = hi + data2;
-    delay(1);
-    dataAvailable = true;
+    msg[msg_pos++] = Serial.read(); // Reading Serial Data and saving in data variable
+    delayMicroseconds(174U);        // 174µs/char @ 57600 Bauds
   }
+
+  msg[msg_pos++] = 0; // add Null byte at the end
+
+  // msg min size 40
+  // msg always start with "20;"
+  if ((msg_pos > 40) && (msg[0] == '2') && (msg[1] == '0') && (msg[2] == ';'))
+  {
+    if (!client.connected()) reconnect();
+    //client.loop();
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish(MQTT_TOPIC_OUT, msg);
+  }
+  else delay(7); // Wait for a complete minimal message
+  
   delay(1); // To Enable some more Modem Sleep
-
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-
-  // convert hi to char array
-  if (dataAvailable == true) {
-    int str_len = hi.length() + 1;
-    char msg[str_len];
-    hi.toCharArray(msg, str_len);
-    //Serial.print("Publish message: ");
-    //Serial.println(msg);
-    client.publish(MQTT_TOPIC_REC, msg);
-    hi = "";
-    dataAvailable = false;
-  }
 }
